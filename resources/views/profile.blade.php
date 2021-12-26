edcf66da6d31ed520793e9568467b96f35c0b324
Artyom <artyom2015pushin@yandex.ru>
2021-12-27 02:22:52 +0400

    fixed the display of editCV adn createCV buttons

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,6 +9,10 @@
     @isset($data)
         @foreach($data as $itemVacancy)
             @include('inc.itemVacancy')
+            <a href="{{route('editCV', $itemVacancy->id)}}"><button class="btn btn-primary">Редактировать резюме </button></a>
         @endforeach
     @endisset
+    @empty($data)
+        <a href="{{route('createCV')}}"><button class="btn btn-primary">Создать резюме</button></a>
+    @endempty
 @endsection
